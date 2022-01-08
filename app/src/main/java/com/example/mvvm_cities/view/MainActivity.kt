66a56a2614a12a57adb06ff44f536ac7a058391d
lindamol <lindamoljosem@gmail.com>
2022-01-07 22:10:28 -0500

    Created MVVM packages

--- app/src/main/java/com/example/mvvm_cities/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_cities/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvm_cities
+package com.example.mvvm_cities.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm_cities.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
